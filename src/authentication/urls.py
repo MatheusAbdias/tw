f77ebc714886b0b7fe6b821633860a5303decf82
Matheus Abdias <matheusabdias@b2bit.company>
2023-11-29 20:29:01 -0300

    feat: :sparkles: Adding login and refresh token view

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from authentication.views import SignupViewSet
+from authentication.views import LoginViewSet, RefreshTokenVIew, SignupViewSet
 
 app_name = "authentication"
 
 router = routers.DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
+router.register("login", LoginViewSet, basename="login")
+router.register("refresh", RefreshTokenVIew, basename="refresh-token")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,6 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    TokenRefreshSerializer,
+)
 
 from authentication.serializers import SignupUserSerializer
 
@@ -8,3 +13,25 @@ from authentication.serializers import SignupUserSerializer
 class SignupViewSet(CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = SignupUserSerializer
     permission_classes = [AllowAny]
+
+
+class LoginViewSet(viewsets.GenericViewSet):
+    serializer_class = TokenObtainPairSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class RefreshTokenVIew(viewsets.GenericViewSet):
+    serializer_class = TokenRefreshSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
